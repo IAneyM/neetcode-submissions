c0e7bb22c6e5bd12cd4eca7e5c507a90aebdd9e4
Aney Moon <112859138+IAneyM@users.noreply.github.com>
2026-07-03 13:51:23 -0400

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        index = -1
+
+        for i in range(len(nums)):
+            if target == nums[i]:
+                index = i
+        
+        return(index)
+
+
+        
